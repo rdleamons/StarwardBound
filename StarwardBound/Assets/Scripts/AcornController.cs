68be06438926b1ec26137b5364102cb056273f88
Rebekah <rdleamons@gmail.com>
2021-01-29 21:11:08 -0600

    Objects spawning

--- StarwardBound/Assets/Scripts/AcornController.cs
+++ StarwardBound/Assets/Scripts/AcornController.cs
@@ -6,29 +6,28 @@ public class AcornController : MonoBehaviour
 {
     public Camera cam;
     private float maxWidth;
-    public GameObject ball;
+    public GameObject acorn;
 
     // Use this for initialization
     void Start()
     {
-        if (cam == null)
-        {
-            cam = Camera.main;
-            Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
-            Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
-            float ballWidth = ball.GetComponent<Renderer>().bounds.extents.x;
-            maxWidth = targetWidth.x - ballWidth;
-            StartCoroutine(Spawn());
-        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0.0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float ballWidth = acorn.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - ballWidth;
+        StartCoroutine(Spawn());
     }
 
     IEnumerator Spawn()
     {
         yield return new WaitForSeconds(2.0f);
-        
-        Vector3 spwanPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
-        Quaternion spawnRotation = Quaternion.identity;
-        Instantiate(ball, spwanPosition, spawnRotation);
-        yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+
+        while (true)
+        {
+            Vector3 spwanPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0.0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(acorn, spwanPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        }
     }
 }
